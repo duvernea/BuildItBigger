3e36d1351c3afa882bc137635a546569d52e308f
Brian DuVerneay <duverneay@gmail.com>
2015-12-05 19:17:47 -0600

    Step 1: Create a Java Library COMPLETE

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,61 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.bduverneay.JokeProvider;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Context mContext;
+
+    private Button mJokeButton;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        mContext = getActivity();
+
+        mJokeButton = (Button) root.findViewById(R.id.tell_joke_button);
+
+        JokeProvider jokeProvider = new JokeProvider();
+        final String testJoke = jokeProvider.getJoke();
+        mJokeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, testJoke, Toast.LENGTH_LONG).show();
+            }
+        });
+        // Log.d(TAG, "Java return int: " + a);
+
+
+        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+        return root;
+    }
+}
---
+++ javajokes/src/main/java/com/bduverneay/JokeProvider.java
@@ -0,0 +1,9 @@
+package com.bduverneay;
+
+public class JokeProvider {
+
+
+    public String getJoke() {
+        return "Why did the chicken cross the road";
+    }
+}
